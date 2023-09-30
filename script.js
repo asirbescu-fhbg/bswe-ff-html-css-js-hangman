308e3adc757bf0d38eb84bded4978b00b0ad9906
Matschoen <146526149+Matschoen@users.noreply.github.com>
2023-09-30 15:07:11 +0000

    Add guess check

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const wortEingabe = document.querySelector(".wort");
 const eingabe = document.querySelector(".eingabe");
 const raten = document.getElementById("raten");
 const vorschlag = document.querySelector(".vorschlag");
+const vorschlageEingabe = document.querySelector(".enter");
+
 var wort;
 
 function wortEingegeben() {
-    wort = document.querySelector(".wort").value;
+    wort = document.querySelector(".wort").value.toLowerCase();
     wortEingabe.style.display = "none";
     eingabe.style.display = "none";
     vorschlag.style.display = "block";
-    
+    vorschlageEingabe.style.display = "block";
+}
+
+function vorschlagAbgegeben() {
+    var buchstabe =  vorschlag.value.toLowerCase();
+    for (let i = 0; i < wort.length; i++) {
+        if (buchstabe == wort[i]) {
+            alert("YES");
+            break;
+        }
+    }
 }
 
 eingabe.addEventListener("click", wortEingegeben);
+vorschlageEingabe.addEventListener("click", vorschlagAbgegeben)

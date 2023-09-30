f72c12cc1288c2094fd02275866908e6946b6950
Matschoen <146526149+Matschoen@users.noreply.github.com>
2023-09-30 13:49:29 +0000

    Update guess function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const wortEingabe = document.querySelector(".wort");
 const eingabe = document.querySelector(".eingabe");
+const raten = document.getElementById("raten");
 
 function getText(){
     var wort = document.querySelector(".wort").value;
-    console.log(wort);
     wortEingabe.style.display = "none";
+    eingabe.style.display = "none";
+    for (let i = 0; i < wort.length; i++) {
+        raten.innerHTML += "<input type='text'>";
+    }
 }
 
 eingabe.addEventListener("click", getText);

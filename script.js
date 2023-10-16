003ccef581b07bfbc66203d4d5916bcbfd40cfe5
asirbescu-fhbg <2210859011@fh-burgenland.at>
2023-10-16 13:11:06 +0200

    Update

--- script.js
+++ script.js
@@ -51,33 +51,9 @@ function richtigErraten(buchstabe) {
 }
 
 function falschErraten() {
-    dropBody();
-    $("#rEyes").addClass("hide");
-    $("#xEyes").removeClass("hide");
+    document.querySelector(".svg").style.display = "inline";
 }
 
-function dropBody() {
-    document.getElementById("#door1").velocity({rotateZ: 90}, 1000);
-    $("#door2").velocity({rotateZ: -90}, 1000);
-    fall();  
-  }
-  
-  
-  function fall() {
-    let dur = 500;
-    let del = 1000;
-    $("#body").velocity({translateY: "200px"}, {duration: dur, delay: del});
-    $("#rope").velocity({y2: "+=200px"}, {duration: dur, delay: del});
-    $("#armL").velocity({y2: "-=60px"}, {duration: dur, delay: del});
-    $("#armR").velocity({y2: "-=60px"}, {duration: dur, delay: del});
-    
-    finish();
-  }
-  
-  function finish () {
-    $("#armL").velocity({y2: "+=70px", x2: "+=10px"}, 500);
-    $("#armR").velocity({y2: "+=70px", x2: "-=10px"}, 500);
-  }
 
 rateEingabeBtn.addEventListener("click", wortEingegeben);
 
